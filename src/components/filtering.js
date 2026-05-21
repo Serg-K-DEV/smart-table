ba250fa066ab085c79ff517fd8468ff9dba3e3c4
Sergey Kostygov <kostygov.sergey@yandex.ru>
2026-05-21 03:19:59 +0300

    Version for PR7

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,37 +1,48 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
-
 // @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
+export function initFiltering(elements) {
+  
+  const updateIndexes = (elements, indexes) => {
+      // @todo: #4.1 — заполнить выпадающие списки опциями  
+      Object.keys(indexes).forEach((elementName) => {
+          elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+              const el = document.createElement('option');
+              el.textContent = name;
+              el.value = name;
+              return el;
+          }))
+      })
+    }
 
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)                                                    // Получаем ключи из объекта
-      .forEach((elementName) => {                                           // Перебираем по именам
-        elements[elementName].append(                                       // в каждый элемент добавляем опции
-            ...Object.values(indexes[elementName])                          // формируем массив имён, значений опций
-                      .map(name => {                                        // используйте name как значение и текстовое содержимое
-                        const elem = document.createElement('option');      // @todo: создать и вернуть тег опции
-                        elem.textContent = name;
-                        elem.value = name;
-                        return elem;
-                      })
-        )
-     })
 
-     return (data, state, action) => {
-          // @todo: #4.2 — обработать очистку поля
-          if (action && action.name === 'clear') {
-              const input = action.parentElement.querySelector('input');  // Найдём поле, связанное с кнопкой очистки (предполагаем, что оно внутри того же родителя)
-              const field = action.dataset.field;
-              if (input) {
-                input.value = '';    // Очистим его
-              }
-              if (field && state[field] !== undefined) {
-                state[field] = '';  // И в состоянии тоже очистим, чтобы фильтрация отработала правильно
-              }}
-
-          // @todo: #4.5 — отфильтровать данные используя компаратор
-          return data.filter(row => compare(row, state));
+  const applyFiltering = (query, state, action) => {
+      // @todo: #4.2 — обработать очистку поля
+      if (action && action.name === 'clear') {
+          const input = action.parentElement.querySelector('input');  // Найдём поле, связанное с кнопкой очистки (предполагаем, что оно внутри того же родителя)
+          const field = action.dataset.field;
+          if (input) {
+            input.value = '';    // Очистим его
           }
-  }
+          if (field && state[field] !== undefined) {
+            state[field] = '';  // И в состоянии тоже очистим, чтобы фильтрация отработала правильно
+          }}
+
+      // @todo: #4.5 — отфильтровать данные используя компаратор
+      const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { // ищем поля ввода в фильтре с непустыми данными
+                    filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
+                }
+            }
+        })
+
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query; // если в фильтре что-то добавилось, применим к запросу
+    }
+
+    return {
+        updateIndexes,
+        applyFiltering
+    }
+
+}
 
